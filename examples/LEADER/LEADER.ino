ea998a063891207f6af0a53d7528ef399a707e45
OPT-OUT <dotcranky+work@gmail.com>
2026-02-23 20:12:54 +0100

    Update LEADER.ino

--- examples/LEADER/LEADER.ino
+++ examples/LEADER/LEADER.ino
@@ -1,13 +1,19 @@
 #include <LEADER.h>
-
-OSCLeader bridge;
+OSCLeader leader;
 
 void setup() {
-  Serial.begin(1000000); 
-  bridge.begin(Serial, 1000000, 1, false);
-  bridge.setIndicator(2); // LED on pin 2 flashes on traffic
+  Serial.begin(230400); //enough is enough. pd[comport] limit seems to be 230400   
+  // Initialize the LEADER on Channel 1, autoHop = false
+  leader.begin(Serial, 230400, 1, false); 
+  // Enable the built-in LED, blink for 40ms, using active-LOW (true for XIAO...)
+  // blink when LEADER is sending data to FOLLOWERS
+  leader.setIndicator(LED_BUILTIN, 40, true);
 }
 
 void loop() {
-  bridge.update();
+// The library handles all the data, routing, and LED blinking internally!
+  leader.update();
+
+  // CIAO! :O)
+
 }
